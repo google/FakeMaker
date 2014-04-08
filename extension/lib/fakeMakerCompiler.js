327c84f6e22e0db734064d14948c316cd3c36d60
johnjbarton <johnjbarton@johnjbarton.com>
2014-04-08 15:48:55 -0700

    Wrap transcode body in a try block; fall back to input src for compile errors

--- extension/lib/fakeMakerCompiler.js
+++ extension/lib/fakeMakerCompiler.js
@@ -12,34 +12,39 @@ import {ScopeAttacher} from './ScopeAttacher';
 import {dumpeur} from './dumpeur';
 
 window.transcode = function transcode(src, url, fncName) {
-    url = fileNamer(src, url, fncName);
-    if (!url)
-      return;
-    var file = new SourceFile(url, src);
-    var tree = new Parser(file).parseScript(true);
-    var global = window; // This is the preprocessor's window.
-    var reporter = new ErrorReporter();
-    var globalScope = ScopeAttacher.attachScopes(reporter, tree, global);
-    var prefixGlobalsWithWindowProxy = new AttachWindowProxyToGlobalsTransformer();
-    var resultTree = prefixGlobalsWithWindowProxy.transformAny(tree);
-    if (!window.transcode.noTracing) {
-        var tracer = new TraceFunctionsTransformer(url);
-        resultTree = tracer.transformAny(resultTree);
+    try {
+        url = fileNamer(src, url, fncName);
+        if (!url)
+          return;
+        var file = new SourceFile(url, src);
+        var tree = new Parser(file).parseScript(true);
+        var global = window; // This is the preprocessor's window.
+        var reporter = new ErrorReporter();
+        var globalScope = ScopeAttacher.attachScopes(reporter, tree, global);
+        var prefixGlobalsWithWindowProxy = new AttachWindowProxyToGlobalsTransformer();
+        var resultTree = prefixGlobalsWithWindowProxy.transformAny(tree);
+        if (!window.transcode.noTracing) {
+            var tracer = new TraceFunctionsTransformer(url);
+            resultTree = tracer.transformAny(resultTree);
+        }
+        var outURL = fileRenamer(url);
+        var encodedSrc = '';
+        if (!window.transcode.noEncodeSource) {
+            encodedSrc += 'window.__F_srcs = window.__F_srcs || [];';
+            encodedSrc += 'window.__F_srcs.push({url: \"' + url + '\", src: \"' +window.btoa(src) + '\"});\n';
+        }
+        var optionsInfo = '//';
+        optionsInfo += ' noTracing: ' + window.transcode.noTracing;
+        optionsInfo += ' noEncodeSource: ' + window.transcode.noEncodeSource;
+        optionsInfo += ' noSourceURL: ' + window.transcode.noSourceURL;
+        var srcURL = '';
+        if (!window.transcode.noSourceURL) {
+            srcURL += '\n//# sourceURL=' + outURL + '\n';
+        }
+        var result = TreeWriter.write(resultTree, {}) + encodedSrc + optionsInfo + srcURL;
+        return result;
+    } catch (ex) {
+        console.log('transcode fails, sending original source for ' + url);
+        return src;
     }
-    var outURL = fileRenamer(url);
-    var encodedSrc = '';
-    if (!window.transcode.noEncodeSource) {
-        encodedSrc += 'window.__F_srcs = window.__F_srcs || [];';
-        encodedSrc += 'window.__F_srcs.push({url: \"' + url + '\", src: \"' +window.btoa(src) + '\"});\n';
-    }
-    var optionsInfo = '//';
-    optionsInfo += ' noTracing: ' + window.transcode.noTracing;
-    optionsInfo += ' noEncodeSource: ' + window.transcode.noEncodeSource;
-    optionsInfo += ' noSourceURL: ' + window.transcode.noSourceURL;
-    var srcURL = '';
-    if (!window.transcode.noSourceURL) {
-        srcURL += '\n//# sourceURL=' + outURL + '\n';
-    }
-    var result = TreeWriter.write(resultTree, {}) + encodedSrc + optionsInfo + srcURL;
-    return result;
 }
