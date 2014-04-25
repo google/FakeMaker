8629da9b2461950435859ef323f0891eae80af22
johnjbarton <johnjbarton@johnjbarton.com>
2014-04-25 11:01:26 -0700

    Personal shell script with required command line flags

---
+++ extension/http/openFakeMaker.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+PROFILE="--user-data-dir=/work/chrome/fakemaker"
+PARKOUR="--enable-experimental-web-platform-features --enable-javascript-harmony"
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome $PROFILE $PARKOUR
